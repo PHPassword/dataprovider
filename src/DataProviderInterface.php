8672039d1589cc55144586aa04f1aff0cef46e38
Phillip Schleicher <schleicher@nexus-netsoft.com>
2019-01-09 00:03:45 +0100

    Added set/get to interface

--- src/DataProviderInterface.php
+++ src/DataProviderInterface.php
@@ -9,4 +9,8 @@ interface DataProviderInterface
     public function toArray(): array;
 
     public function fromArray(array $data): void;
+
+    public function set(string $name, $value): void;
+
+    public function get(string $name);
 }
